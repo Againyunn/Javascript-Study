9506d6ea48eccd8760503cee4d30a7af46e4fb89
JaeyunJungGeeksloft <76543591+Againyunn@users.noreply.github.com>
2022-11-01 21:39:51 +0900

    Submit: problem3

--- woowa-tech/src/problem3.js
+++ woowa-tech/src/problem3.js
@@ -1,5 +1,32 @@
 function problem3(number) {
-  var answer;
+  let answer = 0;
+
+  // 3/6/9를 포함한 수 식별
+  const checkNum = (type, number) => {
+    for (var i = 1; i <= number; i++) {
+      let changedText = i.toString();
+
+      let checkArr = [...changedText];
+      let index = 0;
+
+      while (index < checkArr.length) {
+        if (checkArr[index].includes(`${type}`)) {
+          answer += 1;
+        }
+        index += 1;
+      }
+    }
+  };
+
+  // 3
+  checkNum(3, number);
+
+  // 6
+  checkNum(6, number);
+
+  // 9
+  checkNum(9, number);
+
   return answer;
 }
 
