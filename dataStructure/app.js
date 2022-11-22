10a548c72737b705d4fc1f06f2531ce0232d529c
Jaeyun Jung <76543591+Againyunn@users.noreply.github.com>
2022-11-23 00:29:50 +0900

    Feat: review linked list(type of linked stack)
    review linked list used javascript, especially linked stack

--- dataStructure/app.js
+++ dataStructure/app.js
@@ -2,6 +2,7 @@ import * as Stack from "./Stack.js";
 import * as Queue from "./Queue.js";
 import * as CircularQueue from "./CircularQueue.js";
 import * as PriorityQueue from "./PriorityQueue.js";
+import * as LinkedStack from "./LinkedStack.js";
 
 /**Stack */
 // Stack.pushItem(1);
@@ -41,12 +42,28 @@ import * as PriorityQueue from "./PriorityQueue.js";
 // console.log(CircularQueue.size());
 
 /** Priority Queue */
-const queue = new PriorityQueue.PriorityQueue();
-queue.enqueue(12);
-queue.enqueue(22);
-queue.enqueue(30);
-console.log("Queue list: ", queue.items);
+// const queue = new PriorityQueue.PriorityQueue();
+// queue.enqueue(12);
+// queue.enqueue(22);
+// queue.enqueue(30);
+// console.log("Queue list: ", queue.items);
 
-while (!queue.isEmpty()) {
-  console.log("MAX PRIORITY: ", queue.dequeue());
-}
+// while (!queue.isEmpty()) {
+//   console.log("MAX PRIORITY: ", queue.dequeue());
+// }
+
+/** LinkedStack */
+const linkedStack = new LinkedStack.LinkedStack();
+
+linkedStack.push(1);
+linkedStack.push(2);
+linkedStack.push(3);
+console.log(linkedStack.peek());
+linkedStack.pop();
+
+console.log(linkedStack.peek());
+linkedStack.push(3);
+linkedStack.push(4);
+console.log(linkedStack.size());
+
+console.log("all linkedStack's data: ", linkedStack.getAllList());
